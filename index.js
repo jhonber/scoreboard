151a53d84f4c0fb299084ef7469873766201fb08
Jhon <jhonb3r@gmail.com>
2018-05-27 17:46:03 -0500

    Added express server

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+var port = process.env.PORT || '3000';
+
+app.listen(port);
