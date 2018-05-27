1ee2da4e492b6fa2f5ce976b7b06679143b8ecac
Jhon <jhonb3r@gmail.com>
2018-05-27 18:06:22 -0500

    Test javascript file

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 var path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
+app.use(express.static('public'))
 var port = process.env.PORT || '3000';
-
 app.listen(port);
